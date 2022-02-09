2cb200a09410f5bb4a313924bf4774cded348999
Lucas Keizur <Lucasckz@yahoo.com>
2022-02-09 08:21:08 -0800

    added the file to process chargers

---
+++ envorso_charging_app/lib/chargeStation.dart
@@ -0,0 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:flutter/material.dart';
+
+//This class will pull and store the collection of chagers
+
+class Chargers {}
