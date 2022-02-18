8e4f3664c369b6aaa7ec81e168a9908914321421
Lucas Keizur <Lucasckz@yahoo.com>
2022-02-18 11:11:51 -0800

    Commit to save changes

--- envorso_charging_app/lib/chargeStation.dart
+++ envorso_charging_app/lib/chargeStation.dart
@@ -23,10 +23,14 @@ class Chargers {
   int maxSize = 10;
   //the max range of the chargers being querried
   double range = 0.042478;
-
+  //the stored list of vehicle chargers
   List<Map<String, dynamic>> chargers = [];
-
-  String userKey = "";
+  //the Array of memberships
+  List<String> memberships = [];
+  //the array of car plugs
+  List<String> carPlug = [];
+  //the map of user info
+  Map<String, dynamic> userInfo = {};
 
   //Constructor
   Chargers() {}
@@ -41,31 +45,108 @@ class Chargers {
         //.where('lon', isLessThan: (lon + range), isGreaterThan: (lon - range))
         .get();
 
-    List<Map<String, dynamic>> res = [];
+    chargers = [];
     for (var docs in querryList.docs) {
       chargers.add(docs.data());
-      res.add(docs.data());
     }
 
-    return res;
+    return chargers;
   }
 
-  //Change the search range of chargers
-  void changeRange(int miles) {
+  //Pulls user data and stores it in the Class feilds.
+  void activateAccount(String key) async {
+    DocumentSnapshot<Map<String, dynamic>> user =
+        await FirebaseFirestore.instance.collection('users').doc(key).get();
+
+    carPlug = user['chargerType']['chargers'];
+    memberships = user['services']['services'];
+  }
+
+  //set the array of car plugs
+  void setCarPlug(List<String> plugs) {
+    carPlug = plugs;
+  }
+
+  //set the array of memberships
+  void setMemberships(List<String> mems) {
+    memberships = mems;
+  }
+
+  //set the search range of chargers
+  void setRange(int miles) {
     range = miles / 69.00;
   }
 
-  void changeMaxSize(int size) {
+  //set the max return size of chargers
+  void setMaxSize(int size) {
     maxSize = size;
+  }
+
+  //Remove from the List all plug types not owned by the user
+  List<Map<String, dynamic>> maskPlugs() {
+    for (var n in chargers) {
+      if (!((n['plug'].contains("CHADEMO") && carPlug.contains("CHAdeMO")) ||
+          (n['plug'].contains("J1772") && carPlug.contains("J1772")) ||
+          (n['plug'].contains("J1772COMBO") &&
+              carPlug.contains("SAE Combo CCS")))) {
+        chargers.remove(n);
+      }
+    }
+    return chargers;
+  }
+
+  //Place all plug types not owned by the user at the back of the list
+  List<Map<String, dynamic>> orderPlugs() {
+    List<Map<String, dynamic>> temp = [];
+    for (var n in chargers) {
+      if (!((n['plug'].contains("CHADEMO") && carPlug.contains("CHAdeMO")) ||
+          (n['plug'].contains("J1772") && carPlug.contains("J1772")) ||
+          (n['plug'].contains("J1772COMBO") &&
+              carPlug.contains("SAE Combo CCS")))) {
+        temp.add(n);
+        chargers.remove(n);
+      }
+    }
+    chargers.addAll(temp);
+    return chargers;
+  }
+
+  //Remove from the List all Membership types not owned by the user
+  List<Map<String, dynamic>> maskServices() {
+    for (var n in chargers) {
+      if (!(memberships.contains(n['network']) ||
+          memberships.contains("Electrical Vehicle Charging Station"))) {
+        chargers.remove(n);
+      }
+    }
+    return chargers;
+  }
+
+  //Place all Membership types not owned by the user at the back of the list
+  List<Map<String, dynamic>> orderServices() {
+    List<Map<String, dynamic>> temp = [];
+    for (var n in chargers) {
+      if (!(memberships.contains(n['network']) ||
+          memberships.contains("Electrical Vehicle Charging Station"))) {
+        temp.add(n);
+        chargers.remove(n);
+      }
+    }
+    chargers.addAll(temp);
+    return chargers;
+  }
+
+  void maskSpeed(List<String> levels) {
+    List<Map<String, dynamic>> temp = [];
+  }
+
+  //order the list of chargers by charging Speed
+  void orderSpeed() {
+    List<Map<String, dynamic>> temp = [];
   }
 
   //order the list of chargers by price
   void orderPrice() async {
-    List<Map<String, dynamic>> temp = [];
-  }
-
-  //order the list of chargers by Charger type
-  void orderCharger() {
     List<Map<String, dynamic>> temp = [];
   }
 
@@ -74,15 +155,8 @@ class Chargers {
     List<Map<String, dynamic>> temp = [];
   }
 
-  //order the list of chargers by membership status
-  void orderMembership(List<String> mems) {}
-
-  //order the list of chargers by charging Speed
-  void orderSpeed() {
-    List<Map<String, dynamic>> temp = [];
-  }
-
-  void printChargers() async {
+  //Debuging tool to print charging station names
+  void printChargers() {
     print(chargers.length);
     for (int i = 0; i < chargers.length; i++) {
       print(chargers.elementAt(i)['name']);
