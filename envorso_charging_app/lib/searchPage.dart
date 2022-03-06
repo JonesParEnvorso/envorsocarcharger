428741b32b36fb48d911d8a3641e2a370ba77911
Joe Corona <jzcorona1@gmail.com>
2022-03-05 20:08:32 -0800

    changes on searchPage.dart

--- envorso_charging_app/lib/searchPage.dart
+++ envorso_charging_app/lib/searchPage.dart
@@ -4,6 +4,8 @@ import 'dart:ui';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:avatar_glow/avatar_glow.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
 import 'settings.dart';
 import 'mapScreen.dart';
 import 'chargeStation.dart';
@@ -56,6 +58,11 @@ class _SearchPage extends State<SearchPage> {
   late FocusNode searchBarFocus;
   late FocusNode micButtonFocus;
   late bool isMicPressed;
+
+  late stt.SpeechToText _speech;
+  bool _isListening = false;
+  String _text = 'Press button and speak';
+  double _confidence = 1.0;
 
 // fills the list with the result from the database
   _fillChargerList() async {
@@ -218,15 +225,16 @@ class _SearchPage extends State<SearchPage> {
                       )),
                   Container(
                       child: ElevatedButton(
-                    onPressed: () {
-                      //speech.MyApp;
-                      speech.main();
-                      setState(() {
-                        isMicPressed = !isMicPressed;
-                      });
-                    },
+                    onPressed: //() {
+                        _listen,
+                    child: Icon(_isListening ? Icons.mic : Icons.mic_none),
+                    // speech.main();
+                    // setState(() {
+                    //   isMicPressed = !isMicPressed;
+                    // });
+                    //},
                     focusNode: micButtonFocus,
-                    child: Icon(isMicPressed ? Icons.mic_none : Icons.mic),
+                    //child: Icon(isMicPressed ? Icons.mic_none : Icons.mic),
                     style: ButtonStyle(
                       shape: MaterialStateProperty.all(const CircleBorder()),
                       padding:
@@ -339,6 +347,29 @@ class _SearchPage extends State<SearchPage> {
           ],
         )));
   }
+
+  void _listen() async {
+    if (!_isListening) {
+      bool available = await _speech.initialize(
+        onStatus: (val) => print('onStatus: $val'),
+        onError: (val) => print('onError: $val'),
+      );
+      if (available) {
+        setState(() => _isListening = true);
+        _speech.listen(
+          onResult: (val) => setState(() {
+            _text = val.recognizedWords;
+            if (val.hasConfidenceRating && val.confidence > 0) {
+              _confidence = val.confidence;
+            }
+          }),
+        );
+      }
+    } else {
+      setState(() => _isListening = false);
+      _speech.stop();
+    }
+  }
 }
 
 class listTilesLocations {
@@ -360,4 +391,96 @@ class listTilesLocations {
       required this.lvl2,
       required this.dcFast,
       required this.plugs});
+}
+
+class SpeechScreen extends StatefulWidget {
+  @override
+  _SpeechScreenState createState() => _SpeechScreenState();
+}
+
+class _SpeechScreenState extends State<SpeechScreen> {
+  // final Map<String, HighlightedWord> _highlights = {
+  //   'Washington': HighlightedWord(
+  //     onTap: () => print('Washington'),
+  //     textStyle: const TextStyle(
+  //       color: Colors.blue,
+  //       fontWeight: FontWeight.bold,
+  //     ),
+  //   ),
+  // };
+
+  late stt.SpeechToText _speech;
+  bool _isListening = false;
+  String _text = 'Press button and speak';
+  double _confidence = 1.0;
+
+  @override
+  void initState() {
+    super.initState();
+    _speech = stt.SpeechToText();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Confidence: ${(_confidence * 100.0).toStringAsFixed(1)}%'),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: AvatarGlow(
+        animate: _isListening,
+        glowColor: Theme.of(context).primaryColor,
+        endRadius: 75.0,
+        duration: const Duration(milliseconds: 2000),
+        repeatPauseDuration: const Duration(milliseconds: 100),
+        repeat: true,
+        child: FloatingActionButton(
+          onPressed: _listen,
+          child: Icon(_isListening ? Icons.mic : Icons.mic_none),
+        ),
+      ),
+      body: SingleChildScrollView(
+        reverse: true,
+        child: Container(
+          padding: const EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 150.0),
+          // child: TextHighlight(
+          //   text: _text,
+          //   words: _highlights,
+          //   textStyle: const TextStyle(
+          //     fontSize: 32.0,
+          //     color: Colors.black,
+          //     fontWeight: FontWeight.w400,
+          //   ),
+          // ),
+        ),
+      ),
+    );
+  }
+
+  void _listen() async {
+    if (!_isListening) {
+      bool available = await _speech.initialize(
+        onStatus: (val) => print('onStatus: $val'),
+        onError: (val) => print('onError: $val'),
+      );
+      if (available) {
+        setState(() => _isListening = true);
+        _speech.listen(
+          onResult: (val) => setState(() {
+            _text = val.recognizedWords;
+            if (val.hasConfidenceRating && val.confidence > 0) {
+              _confidence = val.confidence;
+            }
+          }),
+        );
+      }
+    } else {
+      setState(() => _isListening = false);
+      _speech.stop();
+    }
+  }
+
+  printSomething() {
+    _listen();
+  }
 }
